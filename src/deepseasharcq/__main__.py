4b7c58adb102a02ccaac86e9e9b87c2d1c56ec10
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-06-28 17:08:30 -0600

    updates on the command line usage

--- src/deepseasharcq/__main__.py
+++ src/deepseasharcq/__main__.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import logging
 
 class DeepSeaSharcq:
     def __init__(self):
@@ -11,8 +12,20 @@ class DeepSeaSharcq:
     def evaluate(self, data_path: str):
         return NotImplementedError
 def main():
+    # handle arguments from command line
     my_parser = argparse.ArgumentParser(description='cli for deepseasharcq')
+    my_parser.add_argument('ai_action', type=str, help = 'experiment or train or evaluate')
+    my_parser.add_argument('path', help='path to data to use', type=str)
+    args = my_parser.parse_args()
+
+    # run ai program
     dss = DeepSeaSharcq()
-    
+    if args.ai_action == 'train':
+        dss.train(args.path)
+    elif args.ai_action == 'predict':
+        dss.predict(args.path)
+    else:
+        logging.error('invalid ai_action input: choose "train" or "predict" and provide "path" for data to use')
+
 if __name__ == '__main__':
     main()
